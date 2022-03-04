f8defec298664b10a9fb3e691642d552e369e017
Julien_PROUDY <julien.proudy@gmail.com>
2022-03-04 11:17:57 +0100

    Creation des classes User, Event et Depense

---
+++ app/src/main/java/com/example/partymaker/Event.java
@@ -0,0 +1,17 @@
+package com.example.partymaker;
+
+import java.util.ArrayList;
+
+public class Event {
+
+    private String name;
+    private String date_event;
+    private String hour_event;
+    private String adress;
+    private ArrayList<Depense> ardoise;
+    private User user;
+
+    private String date_create_event;
+    private int id;
+
+}
